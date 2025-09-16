2c88284f66eaae87e1d7330e8f51a9b27a28fcd5
Aldric Libonis <aldric.libonis@gmail.com>
2025-09-16 18:55:38 +0200

    :wrench: (main) binds the port to 0.0.0.0

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 }
 void bootstrap();
